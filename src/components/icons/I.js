3525a22999c96ad30788deb3cdb6b10aca57b4a5
unknown <raunakx251@gmail.com>
2024-07-23 16:45:44 +0530

    reachinbox assignment

---
+++ src/components/icons/I.js
@@ -0,0 +1,343 @@
+import * as React from "react";
+const SvgI = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={301}
+    height={230}
+    fill="none"
+    {...props}
+  >
+    <g filter="url(#i_svg__a)">
+      <ellipse
+        cx={147.528}
+        cy={186.812}
+        fill="#1F88F8"
+        fillOpacity={0.35}
+        rx={110.607}
+        ry={21.337}
+      />
+    </g>
+    <path
+      fill="url(#i_svg__b)"
+      d="M148.743 229.402c63.348 0 114.701-51.353 114.701-114.701S212.091 0 148.743 0 34.042 51.353 34.042 114.701s51.353 114.701 114.701 114.701"
+    />
+    <g filter="url(#i_svg__c)">
+      <path
+        fill="url(#i_svg__d)"
+        d="m244.824 77.624-98.291 68.595-98.29-68.595 89.977-62.819a14.55 14.55 0 0 1 16.635 0l17.948 12.548 4.699 3.263 47.318 33.041 4.985 3.5z"
+      />
+    </g>
+    <path fill="url(#i_svg__e)" d="m130.551 135.066-82.308 58.528V77.624z" />
+    <path fill="url(#i_svg__f)" d="m162.523 135.066 82.309 58.528V77.624z" />
+    <path
+      fill="url(#i_svg__g)"
+      d="m48.243 193.594 89.871-63.896a14.53 14.53 0 0 1 16.847 0l89.863 63.896z"
+    />
+    <g filter="url(#i_svg__h)">
+      <path
+        fill="url(#i_svg__i)"
+        d="M58.628 56.61a141.6 141.6 0 0 1-47.835 12.818c2.24 5.206 18.152 31.265 65.388 52.737 1.277.196 30.773-13.526 46.961-22.936-.55.786-50.281-17.513-64.514-42.62"
+      />
+    </g>
+    <path
+      fill="#C9DFFF"
+      d="M59.794 69.566a2.7 2.7 0 0 0-2.374-.351l-26.365 8.53a1.402 1.402 0 0 0-.645 2.23c.512.615 1.35.85 2.106.59l27.036-9.26a.984.984 0 0 0 .242-1.739M59.337 80.071a2.49 2.49 0 0 0-2.498-.544l-18.251 6.142a1.212 1.212 0 0 0-.55 1.92c.442.535 1.168.742 1.825.518l19.146-6.52a.905.905 0 0 0 .328-1.516M69.133 85.273a2.56 2.56 0 0 0-2.476-.408l-21.024 7.64a1.22 1.22 0 0 0-.47 1.986c.46.489 1.168.655 1.799.424l21.91-8.04a.932.932 0 0 0 .261-1.602M76.483 101.044a2.34 2.34 0 0 0-2.565-.615l-12.015 4.585a1.193 1.193 0 0 0-.534 1.824 1.64 1.64 0 0 0 1.913.556l12.878-4.988a.85.85 0 0 0 .323-1.362"
+    />
+    <g filter="url(#i_svg__j)">
+      <path
+        fill="url(#i_svg__k)"
+        d="M232.812 92.95a171.8 171.8 0 0 0 58.003 15.544c-2.715 6.312-22.01 37.91-79.286 63.946-1.549.238-37.315-16.401-56.943-27.811.667.953 60.968-21.236 78.226-51.678"
+      />
+    </g>
+    <path
+      fill="#C9DFFF"
+      d="M231.399 108.661a3.28 3.28 0 0 1 2.879-.426l31.968 10.343a1.7 1.7 0 0 1 .783 2.706 2.34 2.34 0 0 1-2.553.714l-32.784-11.228a1.193 1.193 0 0 1-.293-2.109M222.448 117.175a3.02 3.02 0 0 1 3.029-.661l22.131 7.448a1.47 1.47 0 0 1 .666 2.328c-.535.649-1.416.9-2.212.629l-23.216-7.905a1.098 1.098 0 0 1-.398-1.839M213.25 124.242a3.1 3.1 0 0 1 3.003-.495l25.492 9.265a1.48 1.48 0 0 1 .571 2.407 2.03 2.03 0 0 1-2.182.514l-26.567-9.749a1.13 1.13 0 0 1-.317-1.942M192.552 138.382a2.83 2.83 0 0 1 3.11-.746l14.569 5.559a1.447 1.447 0 0 1 .648 2.212 1.99 1.99 0 0 1-2.319.674l-15.616-6.048a1.03 1.03 0 0 1-.392-1.651M202.062 130.831a3.3 3.3 0 0 1 2.882-.304l33.568 12.274a1.502 1.502 0 0 1 .473 2.542 2.07 2.07 0 0 1-2.073.383l-34.628-12.756c-.933-.344-1.065-1.611-.222-2.139M83.754 88.72a2.72 2.72 0 0 0-2.377-.25L53.693 98.592a1.24 1.24 0 0 0-.39 2.097c.47.41 1.126.531 1.71.316l28.558-10.52a.988.988 0 0 0 .183-1.765"
+    />
+    <path
+      stroke="url(#i_svg__l)"
+      strokeWidth={2.613}
+      d="M243.764 26.999a4.79 4.79 0 1 1-9.58-.001 4.79 4.79 0 0 1 9.58 0Z"
+    />
+    <circle
+      cx={69.145}
+      cy={204.666}
+      r={4.79}
+      stroke="url(#i_svg__m)"
+      strokeWidth={2.613}
+    />
+    <circle
+      cx={216.33}
+      cy={214.246}
+      r={4.79}
+      stroke="url(#i_svg__n)"
+      strokeWidth={2.613}
+    />
+    <path
+      fill="url(#i_svg__o)"
+      d="M85.402 31.932c.515-2.133 3.549-2.133 4.064 0l1.733 7.179a2.09 2.09 0 0 0 1.541 1.541l7.179 1.733c2.133.515 2.133 3.549 0 4.064l-7.179 1.733a2.09 2.09 0 0 0-1.541 1.541l-1.733 7.179c-.515 2.133-3.55 2.133-4.064 0l-1.733-7.179a2.09 2.09 0 0 0-1.541-1.541l-7.18-1.733c-2.132-.515-2.132-3.549 0-4.064l7.18-1.733a2.09 2.09 0 0 0 1.541-1.541z"
+    />
+    <path
+      fill="url(#i_svg__p)"
+      d="M259.912 183.417c.515-2.133 3.549-2.133 4.064 0l1.733 7.179a2.09 2.09 0 0 0 1.541 1.541l7.179 1.733c2.133.515 2.133 3.549 0 4.064l-7.179 1.733a2.09 2.09 0 0 0-1.541 1.541l-1.733 7.179c-.515 2.133-3.549 2.133-4.064 0l-1.733-7.179a2.09 2.09 0 0 0-1.541-1.541l-7.179-1.733c-2.133-.515-2.133-3.549 0-4.064l7.179-1.733a2.09 2.09 0 0 0 1.541-1.541z"
+    />
+    <path
+      fill="url(#i_svg__q)"
+      d="M37.276 166.614c1.324-.212 2.38 1.089 1.902 2.342l-3.229 8.455c-.478 1.253-2.133 1.518-2.979.477l-5.708-7.024c-.845-1.041-.247-2.606 1.077-2.818z"
+    />
+    <path
+      fill="url(#i_svg__r)"
+      d="M263.371 80.422c-.332-1.3.868-2.47 2.159-2.107l8.713 2.448c1.291.363 1.705 1.987.745 2.924l-6.477 6.321c-.959.937-2.573.484-2.904-.816z"
+    />
+    <defs>
+      <linearGradient
+        id="i_svg__b"
+        x1={147.859}
+        x2={150.209}
+        y1={-37.349}
+        y2={365.463}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#21262D" />
+        <stop offset={1} stopColor="#BCD8FF" />
+      </linearGradient>
+      <linearGradient
+        id="i_svg__d"
+        x1={146.533}
+        x2={146.533}
+        y1={12.193}
+        y2={146.219}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#EDF4FF" />
+        <stop offset={1} stopColor="#ABCCFF" />
+      </linearGradient>
+      <linearGradient
+        id="i_svg__e"
+        x1={89.397}
+        x2={89.397}
+        y1={77.624}
+        y2={193.594}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#ABCCFF" />
+        <stop offset={1} stopColor="#70A8FF" />
+      </linearGradient>
+      <linearGradient
+        id="i_svg__f"
+        x1={203.678}
+        x2={203.678}
+        y1={77.624}
+        y2={193.594}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#ABCCFF" />
+        <stop offset={1} stopColor="#70A8FF" />
+      </linearGradient>
+      <linearGradient
+        id="i_svg__g"
+        x1={146.533}
+        x2={146.533}
+        y1={127.006}
+        y2={193.594}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#70A8FF" />
+        <stop offset={1} stopColor="#5597FD" />
+      </linearGradient>
+      <linearGradient
+        id="i_svg__i"
+        x1={22.986}
+        x2={96.579}
+        y1={85.786}
+        y2={71.415}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#C6DDFF" />
+        <stop offset={1} stopColor="#fff" />
+      </linearGradient>
+      <linearGradient
+        id="i_svg__k"
+        x1={276.031}
+        x2={186.795}
+        y1={128.328}
+        y2={110.903}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#C6DDFF" />
+        <stop offset={1} stopColor="#fff" />
+      </linearGradient>
+      <linearGradient
+        id="i_svg__l"
+        x1={231.48}
+        x2={250.532}
+        y1={23.188}
+        y2={39.7}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#94BFFF" />
+        <stop offset={1} stopColor="#4C94FE" />
+      </linearGradient>
+      <linearGradient
+        id="i_svg__m"
+        x1={61.651}
+        x2={80.703}
+        y1={200.856}
+        y2={217.367}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#94BFFF" />
+        <stop offset={1} stopColor="#4C94FE" />
+      </linearGradient>
+      <linearGradient
+        id="i_svg__n"
+        x1={208.837}
+        x2={227.888}
+        y1={210.436}
+        y2={226.947}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#94BFFF" />
+        <stop offset={1} stopColor="#4C94FE" />
+      </linearGradient>
+      <linearGradient
+        id="i_svg__o"
+        x1={87.434}
+        x2={87.434}
+        y1={23.515}
+        y2={65.319}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#70A8FF" />
+        <stop offset={1} stopColor="#5597FD" />
+      </linearGradient>
+      <linearGradient
+        id="i_svg__p"
+        x1={261.944}
+        x2={261.944}
+        y1={175}
+        y2={216.804}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#70A8FF" />
+        <stop offset={1} stopColor="#5597FD" />
+      </linearGradient>
+      <linearGradient
+        id="i_svg__q"
+        x1={40.255}
+        x2={26.737}
+        y1={166.137}
+        y2={177.122}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#70A8FF" />
+        <stop offset={1} stopColor="#5597FD" />
+      </linearGradient>
+      <linearGradient
+        id="i_svg__r"
+        x1={262.625}
+        x2={274.791}
+        y1={77.499}
+        y2={89.964}
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stopColor="#70A8FF" />
+        <stop offset={1} stopColor="#5597FD" />
+      </linearGradient>
+      <filter
+        id="i_svg__a"
+        width={264.76}
+        height={86.221}
+        x={15.148}
+        y={143.702}
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+        <feGaussianBlur
+          result="effect1_foregroundBlur_1_12838"
+          stdDeviation={10.886}
+        />
+      </filter>
+      <filter
+        id="i_svg__c"
+        width={203.549}
+        height={140.993}
+        x={44.759}
+        y={12.193}
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          result="hardAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        />
+        <feOffset dy={3.484} />
+        <feGaussianBlur stdDeviation={1.742} />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0" />
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_1_12838" />
+        <feBlend
+          in="SourceGraphic"
+          in2="effect1_dropShadow_1_12838"
+          result="shape"
+        />
+      </filter>
+      <filter
+        id="i_svg__h"
+        width={133.251}
+        height={86.459}
+        x={0.342}
+        y={49.642}
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          result="hardAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        />
+        <feOffset dy={3.484} />
+        <feGaussianBlur stdDeviation={5.226} />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix values="0 0 0 0 0.0196078 0 0 0 0 0.168627 0 0 0 0 0.396078 0 0 0 0.15 0" />
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_1_12838" />
+        <feBlend
+          in="SourceGraphic"
+          in2="effect1_dropShadow_1_12838"
+          result="shape"
+        />
+      </filter>
+      <filter
+        id="i_svg__j"
+        width={165.841}
+        height={109.103}
+        x={134.5}
+        y={91.209}
+        colorInterpolationFilters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood floodOpacity={0} result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          result="hardAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        />
+        <feOffset dx={-5.28} dy={13.064} />
+        <feGaussianBlur stdDeviation={7.403} />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix values="0 0 0 0 0.021441 0 0 0 0 0.168056 0 0 0 0 0.395833 0 0 0 0.15 0" />
+        <feBlend in2="BackgroundImageFix" result="effect1_dropShadow_1_12838" />
+        <feBlend
+          in="SourceGraphic"
+          in2="effect1_dropShadow_1_12838"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>
+);
+export default SvgI;
---
+++ src/components/icons/index.js
@@ -0,0 +1,5 @@
+export { default as Google } from "./Google";
+export { default as I } from "./I";
+export { default as Logo } from "./Logo";
+export { default as Mail } from "./Mail";
+export { default as React } from "./React";
